332def957e7361cfaa50e24468965cde9deaff52
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:47:19 +0300

    [Package] Remove unsafe flags

--- Package.swift
+++ Package.swift
@@ -47,11 +47,7 @@ func addTest(target: String, name: String) {
         .executableTarget(
             name: "Tests/\(target)/\(name)",
             dependencies: ["Test", "MIDI", "Music"],
-            path: "Tests/\(target)/\(name)",
-            swiftSettings: [
-                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
-                .unsafeFlags(["-Xfrontend", "-enable-experimental-concurrency"])
-            ]))
+            path: "Tests/\(target)/\(name)"))
 }
 
 // MARK: - custom package source
