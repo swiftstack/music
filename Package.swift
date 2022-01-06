cf363884bb0db033000794fb77074857ffffa88c
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:47:25 +0300

    [Package] Add trailing comma

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["MIDI"]),
     ],
     dependencies: [
-        .package(name: "Test")
+        .package(name: "Test"),
     ],
     targets: [
         .target(
