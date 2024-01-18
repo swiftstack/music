6f8e674c95d9c31fc7a64560336c9c8a4e39cb56
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-01-18 10:53:14 +0300

    [Package] Bump darwin platforms

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "Music",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
+        .iOS(.v16),
+        .macOS(.v13),
     ],
     products: [
         .library(
