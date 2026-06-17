29a2faa5e5b2876e37a3dae2fef798a52b7c5254
Tony Freeman <tonyfreeman@users.noreply.github.com>
2026-06-17 21:50:12 +0300

    Bump versions

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.4
 import PackageDescription
 
 let package = Package(
     name: "Music",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13),
+        .iOS(.v26),
+        .macOS(.v26),
     ],
     products: [
         .library(
